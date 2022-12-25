254d69802c5ecea23332367240f52afd023647d2
ThopsD <74445064+thops1925@users.noreply.github.com>
2022-12-26 02:26:32 +0800

    metanext13

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,19 @@
 'use client';
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { fadeIn, textContainer } from '../utils/motion';
+
+export const TypingText = ({ title, textStyles }) => (
+  <motion.p
+    variants={textContainer}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+  >
+    Typing Text
+  </motion.p>
 );
 
-export const TitleText = () => (
+export const TitleText = ({ title, textStyles }) => (
   <h2>Title Text</h2>
 );
--- sections/About.jsx
+++ sections/About.jsx
@@ -6,7 +6,18 @@ import styles from '../styles';
 import { fadeIn, staggerContainer } from '../utils/motion';
 
 const About = () => (
-  <section>
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0 " />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col `}
+    >
+      <TypingText title="|About Meta" textStyles="text-center" />
+    </motion.div>
+
     About section
   </section>
 );
