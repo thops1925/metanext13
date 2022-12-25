1d8e1b501b35d4a4b15bd9ae00c5461533d616f0
ThopsD <74445064+thops1925@users.noreply.github.com>
2022-12-26 00:40:41 +0800

    motion

---
+++ sections/About.jsx
@@ -0,0 +1,14 @@
+'use client';
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => (
+  <section>
+    About section
+  </section>
+);
+
+export default About;
