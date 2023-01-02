70d4deaeabdccbfbbd6d558217e15abadefc094a
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-01-03 01:49:42 +0800

    whats new

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -16,7 +16,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
       <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-25 lg:rotate-[-90deg] lg:origin-[0,0]">{title}</h3>
     ) : (
       // <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-25 lg:rotate-[-90deg] lg:origin-[0,0]">{title}</h3>
-      <div className="absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0,5)] rounded-b-[24px]">
+      <div className="absolute bottom-0 p-7 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px] rounded-t-[24px]  ">
         <div className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px] `}>
           <img src="/headset.svg" alt="headset" className="w-1/2 h-1/2 object-contain " />
         </div>
--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,7 +1,14 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import { StartSteps, TitleText, TypingText } from '../components';
+import { startingFeatures } from '../constants';
+
 const WhatsNew = () => (
-  <section>
+  <section className={`${styles.paddings} relative z-10`}>
     What's new section
   </section>
 );
