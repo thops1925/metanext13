d4795f88f7efe952d09d66c2bfe86ee6cd909006
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-01-03 04:45:27 +0800

    people

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,8 +1,43 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { staggerContainer, fadeIn } from '../utils/motion';
+import { TitleText, TypingText } from '../components';
+
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: 'false', amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex  flex-col }`}
+
+    >
+      <TypingText title="| People of the world" textStyles="text-center" />
+      <TitleText title={<>Look for your friends</>} textStyles="text-center" />
+
+      <motion.div
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        className="relative mt-[60px] flex w-full h-[550px]"
+      >
+        <img src="/map.png" alt="map" className="w-full h-full object-contain" />
+        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="/people-01.png" alt="people" className="w-full h-full" />
+        </div>
+
+        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="/people-02.png" alt="people" className="w-full h-full" />
+        </div>
+
+        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="/people-03.png" alt="people" className="w-full h-full" />
+        </div>
+      </motion.div>
+
+    </motion.div>
   </section>
 );
 
