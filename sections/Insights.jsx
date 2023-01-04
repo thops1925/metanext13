003d0e34e5ed038e8a8aeeb3742f5dfc6b39907a
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-01-05 04:20:47 +0800

    fix

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,10 +1,9 @@
 'use client';
 
-
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
-import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import { staggerContainer } from '../utils/motion';
 import { InsightCard, TitleText, TypingText } from '../components';
 import { insights } from '../constants';
 
@@ -18,7 +17,7 @@ const Insights = () => (
       className={`${styles.innerWidth} mx-auto flex  flex-col }`}
 
     >
-      <TypingText title=" Insight" textStyles="text-center" />
+      <TypingText title="| Insight" textStyles="text-center" />
       <TitleText title="Insight about meta" textStyles="text-center" />
       <div className="mt-[50px] flex flex-col gap-[30px] ">
 
