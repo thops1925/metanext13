916cb0f45691925c8f998b3af4b9c37bbac25640
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-01-09 03:10:07 +0800

    head

--- app/head.js
+++ app/head.js
@@ -2,6 +2,9 @@ const Head = () => (
   <>
     <title>Metanext13</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1315702951113442"
+      crossOrigin="anonymous"
+    />
     <link rel="icon" href="/favicon.ico" />
   </>
 );
